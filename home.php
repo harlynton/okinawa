56f1845de68fabb54274878ed43c041205f70ce0
Harlynton <harlynton.castano@gmail.com>
2023-03-21 14:14:14 -0500

    ajuste para mostrar entradas de blog.

--- home.php
+++ home.php
@@ -1,10 +1,14 @@
 <?php get_header();?>
-
   <main class="pagina seccion no-sidebars contenedor">
     <ul class = "listado-blog">
-    <?php while(have_posts()): the_post(); ?>
-      <?php get_template_part('template-parts/loop', 'blog')?>
-    <?php endwhile; ?>
+
+    <?php
+        while(have_posts()) {
+            the_post();
+            get_template_part('template-parts/blog');
+        }
+    ?>
+
     </ul>
   </main>
 
